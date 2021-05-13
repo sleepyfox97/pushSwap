ff689c8bb7caf548de6616bd7825e0fd618f8cc1
sleepyfox97 <h1649bot@gmail.com>
2021-05-13 17:00:38 +0900

    libft gnl's test isn't done

--- test/test_gnl.c
+++ test/test_gnl.c
@@ -1,6 +1,43 @@
 #include <unistd.h>
-#include <stdio.h>
-#include "../libft/libft.h"
+#include <stdlib.h>
+//#include "../libft/libft.h"
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i] != '\0')
+		i++;
+	return (i);
+}
+
+void	*ft_memmove(void *dst, const void *src, size_t len)
+{
+	unsigned char		*s1;
+	unsigned const char	*s2;
+	size_t				i;
+
+	i = 0;
+	s1 = (unsigned char *)dst;
+	s2 = (unsigned const char *)src;
+	if (len == 0 || s1 == s2)
+		return (dst);
+	if (s1 > s2)
+	{
+		while (len-- > 0)
+			s1[len] = s2[len];
+	}
+	else
+	{
+		while (i < len)
+		{
+			s1[i] = s2[i];
+			i++;
+		}
+	}
+	return (dst);
+}
 
 static size_t	null_strlen(char *s)
 {
@@ -30,9 +67,9 @@ static char	*slide_box(char **box, size_t i)
 	if (nbox == NULL)
 		return (safe_free(box));
 	i++;//ここでi++してるので，注意
-	nbox = ft_memmove(nbox, (box[0] + i), (j - i));//j>i is garantee
+	nbox = (char *)ft_memmove(nbox, (box[0] + i), (j - i));//j>i is garantee
 	nbox[j - i] = '\0'; 
-	free(box[0]);
+	safe_free(box);
 	box[0] = nbox;
 	return (box[0]);
 }
@@ -40,7 +77,6 @@ static char	*slide_box(char **box, size_t i)
 static char	*make_line(char **box)
 {
 	size_t	i;
-	size_t	j;
 	char	*line;
 
 	i = 0;
@@ -49,7 +85,7 @@ static char	*make_line(char **box)
 	line = (char *)malloc(sizeof(char) * (i + 1));
 	if (line == NULL)
 		return (safe_free(box));
-	line = ft_memmove(line, box[0], i);
+	line = (char *)ft_memmove(line, box[0], i);
 	line[i] = '\0';
 	if (box[0][i] == '\0')
 	{
@@ -74,11 +110,27 @@ static char	*free_strjoin(char *s1, char *s2)
 	if (re == NULL)
 		return (NULL);
 	if (s1 != NULL)
-		re = ft_memmove(re, s1, i);
+		re = (char *)ft_memmove(re, s1, i);
 	ft_memmove((re + i), s2, j);
 	re[i + j + 1] = '\0';
 	safe_free(&s1);
 	return (re);
+}
+
+static int	ft_find_newline(char *s)
+{
+	size_t	i;
+
+	i = 0;
+	if (s == NULL)
+		return (0);
+	while (s[i] != '\0')
+	{
+		if (s[i] == '\n')
+			return (1);
+		i++;
+	}
+	return (0);
 }
 
 //不正なfdは渡されないものとする．としたかったが，readの失敗は見た方が良さげ
@@ -91,14 +143,14 @@ int	get_next_line(int fd, char **line)
 	pre = (char *)malloc(sizeof(char) * 100);
 	if (pre == NULL || fd < 0)
 		return (-1);
-	if (!box || ft_strchr(box, '\n'))
+	if (!ft_find_newline(box))
 	{
 		while (1)
 		{
-			buf = read(fd, pre, 100);
+			buf = read(fd, pre, 99);
 			pre[buf] = '\0';
 			box = free_strjoin(box, pre);
-			if (ft_strchr(box, '\n'))
+			if (ft_find_newline(box) || buf == 0)
 				break ;
 		}
 	}
@@ -109,34 +161,4 @@ int	get_next_line(int fd, char **line)
 	if (box == NULL)
 		return (0);
 	return (1);
-}
-
-int main(int argc, char *argv[])
-{
-	// char *box = malloc(11);
-	// size_t	i;
-
-	// i = 0;
-	// while (i < 5)
-	// 	box[i++] = '1';
-	// box[i++] = '\n';
-	// while(i < 10)
-	// 	box[i++] = '2';
-	// printf("box = %s\n", box);
-	// char *line = make_line(&box);
-	// printf("box = %s\n", box);
-	// printf("line = %s\n", line);
-
-	// if (argc != 3)
-	// 	return (0);
-	// char *re;
-
-	// re = free_strjoin(argv[1], argv[2]);
-	// printf("%s\n", re);
-	char *line;
-	while(0 <= get_next_line(0, &line))
-	{
-		printf("%s", line);
-	}
-	return (0);
 }
